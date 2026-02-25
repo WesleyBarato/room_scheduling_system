5e5176f7b2531344eb0f477d07fdb1928e7f95ed
WesleyBarato <wesleycardosoc@gmail.com>
2026-02-25 15:25:33 -0300

    feat: Scheduling rooms with localStorage to test the enviroment

---
+++ src/script.js
@@ -0,0 +1,30 @@
+import { supabase } from "./supabase.js";
+import "./style.css";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+
+  // Faz login no Supabase
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: senha,
+  });
+
+  if (error) {
+    alert("Erro no login: " + error.message);
+    return;
+  }
+
+  // Login deu certo 🎉
+  alert("Login realizado com sucesso!");
+
+  console.log("Usuário logado:", data.user);
+
+  // redireciona pra outra página
+  window.location.href = "dashboard.html";
+});
